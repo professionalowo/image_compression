34a0894ff8d450bb8b15549d235ceceabcb47562
professionalowo <martin.brandl1307@gmail.com>
2024-08-06 19:24:56 +0200

    removed comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,3 @@
-use std::{
-    env, fs,
-    io::{Error, Write},
-};
-
-use png::PngImage;
-
 mod crc;
 mod deflate;
 pub mod png;
@@ -15,47 +8,3 @@ pub trait Compressable {
     where
         Self: Sized;
 }
-
-/*
-fn main() -> Result<(), Error> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        panic!("{}", "Usage: 'image_compression <input> <output>'");
-    }
-    println!("{:#?}", args);
-
-    let input = &args[1];
-    let output = &args[2];
-
-    let bytes_in = fs::read(input)?;
-
-    let png = PngImage::try_create(bytes_in).unwrap();
-
-    println!("{:X?}", &png.file_signature);
-    println!("{:#?}", &png.header);
-
-    let mut outfile = fs::File::create(output)?;
-
-    let compressed = png.try_compress().unwrap();
-
-    let compressed_size = compressed.size();
-
-    let bytes_out: Vec<u8> = compressed.into();
-    outfile.write_all(&bytes_out)?;
-
-    let chunk = &png.get_data()[2];
-
-    println!(
-        "{:X?}\n{},{},{},{}",
-        chunk,
-        chunk.is_ancillary(),
-        chunk.is_private(),
-        chunk.is_reserved(),
-        chunk.is_save_to_copy()
-    );
-
-    println!("Compressed: {} -> {}", png.size(), compressed_size);
-
-    Ok(())
-}
- */
