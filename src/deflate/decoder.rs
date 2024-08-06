1dc10602bab224f44ed9f1a899f3c11b7f674bae
professionalowo <martin.brandl1307@gmail.com>
2024-08-06 20:34:57 +0200

    implemented ByteWindow for deflace decoding

--- src/deflate/decoder.rs
+++ src/deflate/decoder.rs
@@ -1,4 +1,5 @@
-use std::io::Error;
+//see: https://github.com/nayuki/Simple-DEFLATE-decompressor/blob/master/java/src/ByteHistory.java
+use std::{io::Error, ops::Range};
 
 #[derive(Debug)]
 pub struct Decoder {
@@ -15,6 +16,43 @@ impl Decoder {
     }
 
     pub fn decode(&self) -> Result<Box<[u8]>, Error> {
+        let window = ByteWindow::<{ 32 * 1024 }>::new();
         Ok(Box::new([0]))
+    }
+}
+
+struct ByteWindow<const COUNT: usize> {
+    written: u64,
+    index: usize,
+    data: [u8; COUNT],
+}
+
+impl<const COUNT: usize> ByteWindow<COUNT> {
+    pub const fn new() -> Self {
+        Self {
+            written: 0,
+            index: 0,
+            data: [0; COUNT],
+        }
+    }
+
+    pub fn append(&mut self, byte: u8) {
+        self.data[self.index] = byte;
+        self.index = (self.index + 1) % COUNT;
+
+        if self.written < COUNT.try_into().unwrap() {
+            self.written += 1;
+        }
+    }
+
+    pub fn copy(&mut self, distance: usize, length: usize, buf: &mut [u8]) {
+        let mut read_index: usize = ((self.index) - distance - COUNT) % (COUNT);
+        let range: Range<usize> = 0..length;
+        for i in range {
+            let byte: u8 = self.data[read_index];
+            read_index = (read_index + 1) % COUNT;
+            buf[i] = byte;
+            self.append(byte)
+        }
     }
 }
